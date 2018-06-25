7c994d3f726a9e1b1cc46fb040135c0ffbdfd598
Shi Dong <shidong1986@gmail.com>
2018-06-25 16:14:46 -0700

    Some minor modification of shell script to run VGG training

--- runvggimagenet.sh
+++ runvggimagenet.sh
@@ -17,12 +17,12 @@ DATE=$(date +%Y-%m-%d)
 
 TRAIN_DIR=${HOME}/Lab/vgg/train-${DATE}
 if [ ! -d "${TRAIN_DIR}" ]; then
-  mkdir ${TRAIN_DIR}
+  mkdir -p ${TRAIN_DIR}
 fi
 
 python ./scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --model=${MODEL} \
-  --data_name=imagenet --data_dir=/data/imagenet/tf --train_dir=${TRAIN_DIR} \
+  --data_name=imagenet --data_dir=${DATA_DIR}/data/imagenet/tf --train_dir=${TRAIN_DIR} \
   --save_summaries_steps 10 --summary_verbosity=0 --save_model_secs=1000 --print_training_accuracy=True \
-  --display_every 1\
+  --display_every 1000\
   --variable_update=parameter_server --local_parameter_device=cpu --num_batches=${ITERATIONS} \
   --num_gpus=${NGPUS} --batch_size=${BATCH_SIZE} 2>&1 | tee ${TRAIN_DIR}/tf-vgg.txt
